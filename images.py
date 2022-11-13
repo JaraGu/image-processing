2ccc8270daf5d76b8833e62873e4ed5b978f5384
jara <jaraguta03@gmail.com>
2022-11-13 10:39:13 +0300

    smoothed an image

--- images.py
+++ images.py
@@ -1,6 +1,9 @@
-from PIL import Image,ImageFilter
+from PIL import Image, ImageFilter
 
-img=Image.open("./Pokedex/pikachu.jpg")
+img = Image.open("./Pokedex/pikachu.jpg")
 
-filitered_image=img.filter(ImageFilter.BLUR)
-filitered_image.save('blurred.png','png')
+filitered_image = img.filter(ImageFilter.BLUR)
+filitered_image.save('blurred.png', 'png')
+
+filitered_image2 = img.filter(ImageFilter.SMOOTH)
+filitered_image2.save('smooth.png', 'png')
